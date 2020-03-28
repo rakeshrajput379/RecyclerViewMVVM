c73b34712993ebca8bbb48401e4df1e5b8d126c9
rakesh <rakeshrajput3790@gmail.com>
2020-03-28 14:19:35 +0530

    ok
    ok

--- app/src/main/java/com/rakesh/recyclerviewmvvm/MainActivity.kt
+++ app/src/main/java/com/rakesh/recyclerviewmvvm/MainActivity.kt
@@ -2,14 +2,54 @@ package com.rakesh.recyclerviewmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rakesh.recyclerviewmvvm.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityMainBinding
+    lateinit var viewModle : MainActivityVM
+    lateinit var recyclerViewActivityAdapter : RecyclerViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        //rakesh
+        viewModle = ViewModelProviders.of(this).get(MainActivityVM::class.java)
+
+        binding.mainVM = viewModle
+
+        binding.lifecycleOwner  = this
+
+        setAdapater()
+
     }
+
+
+    fun setAdapater(){
+
+        val rvArrayList: ArrayList<MainActivityM> = viewModle.addRecyclerViewData()
+
+        recyclerViewActivityAdapter = RecyclerViewAdapter(rvArrayList,this)
+
+
+        val linearLayoutManager = LinearLayoutManager(this)
+
+        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        binding.root.rv_list.layoutManager = linearLayoutManager
+
+
+        binding.root.rv_list.adapter = recyclerViewActivityAdapter
+
+
+        //  recyclerViewActivityAdapter.notifyDataSetChanged()
+    }
+
+
+
 }
